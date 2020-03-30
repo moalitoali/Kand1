7b6ad5e5df4c6f2db8b41b1a9eb7558b4bf136ef
moalitoali <moalilekander@hotmail.com>
2020-03-30 15:34:22 +0200

    fixade lite snyggt

--- kand1.m
+++ kand1.m
@@ -1,11 +1,11 @@
 %Experimental setup @ tandemlab in uppsala
-d1 = 0.40;                            %distance between MCP1 & MCP2
-d2 = 0.10;                            %distance between MCP2 & Si/detector
+d1 = 0.40;                            % distance between MCP1 & MCP2
+d2 = 0.10;                            % distance between MCP2 & Si/detector
 
-A = 79.904;                           %Ion
-E = 44e6;                             %Energy of ion
+A = 79.904;                           % Ion
+E = 44e6;                             % Energy of ion
 
 v = velocity(E,A);                    % velocity of Ion 
 
-TOF1 = d1/v                             % TOF1
-TOF2 = (d1+d2)/v                        % TOF2
+TOF1 = d1/v;                             % TOF1
+TOF2 = (d1+d2)/v;                        % TOF2
--- velocity.m
+++ velocity.m
@@ -1,11 +1,9 @@
 %Given inputs energy and ion, this function spits out the velocity of ion.
 %Non relativistic 
-
 function v = velocity(E,A)
-u = 1.6605394e-27;          %atomic mass unit
-m = A*u;                    %mass of ion [Kg]
-E = E*1.60217662e-19;       %converting eV to joule
+    u = 1.6605394e-27;          % atomic mass unit
+    m = A*u;                    % mass of ion [Kg]
+    E = E*1.60217662e-19;       % converting eV to joule
 
-v = sqrt((2*E)/m);
-
+    v = sqrt(2*E/m);
 end 
