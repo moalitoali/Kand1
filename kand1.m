9713a033d25bbb34b8cde3945d29656e7aa682f5
moalitoali <moalilekander@hotmail.com>
2020-03-30 15:24:55 +0200

    lagt in alans macro

--- kand1.m
+++ kand1.m
@@ -1,4 +1,11 @@
-% testar lite
+%Experimental setup @ tandemlab in uppsala (?)
+d1 = 0.40;                            %distance between MCP1 & MCP2
+d2 = 0.10;                            %distance between MCP2 & Si/detector
 
-a = 2;
-b = 1;
+A = 79.904;                           %Ion
+E = 44e6;                             %Energy of ion
+
+v = velocity(E,A);                    % velocity of Ion 
+
+t1 = d1/v;
+t2 = (d1+d2)/v;
