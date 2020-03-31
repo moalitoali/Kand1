caa9928b84b69d6ddac429e3d7c0f2e9ca8efee9
moalitoali <moalilekander@hotmail.com>
2020-03-31 14:14:40 +0200

    hej

--- kand1.m
+++ kand1.m
@@ -2,7 +2,7 @@
 d1 = 0.40;                            % distance between MCP1 & MCP2
 d2 = 0.10;                            % distance between MCP2 & Si/detector
 
-A = 79.904;                           % Ion (# neutrons+protons)
+A = 79.904;                           % atomic mass of ion
 E = 44e6;                             % Energy of ion
 %E = [44e6 44e6 44e6];
 %A = [79 79 79];
@@ -12,8 +12,11 @@ v = velocity(E,A);                    % velocity of Ion
 TOF1 = d1./v;                         % TOF1
 TOF2 = (d1+d2)./v;                    % TOF2
 
-x = linspace(0, 50, 1000);
-mean = 25;
+x = linspace(0, 50, 100);
+mean_E = 25;
+%sigma = linspace(0, 50, 50);
 sigma = 5;
-gauss = normpdf(x, mean, sigma);
+gauss = normpdf(x, mean_E, sigma);
+
+figure(1);
 plot(x, gauss);
